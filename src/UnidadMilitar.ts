b850c471633edc44b2f803c888754b56a08abc06
macho991 <nachocremona264@gmail.com>
2026-09-20 22:51:36 -0300

    arreglo de unidad militar

--- src/UnidadMilitar.ts
+++ src/UnidadMilitar.ts
@@ -1,12 +1,17 @@
 export class UnidadMilitar{
     private vida: number; 
 
+    //aca cambie porque estaba mal antes, no era metodo, no estaba creado
     constructor(VidaInicial:number){
-        setVida(VidaInicial)
+        this.vida = VidaInicial
     }
+
+    //tenia un error de tipografia
     public RecibirDisparo(). void{
-        this.vida =this.vida > -1 : 0;
+        this.vida = this.vida > 0 ? this.vida -1 : 0;
     }
+
+    //me falto el ? que es el que evalua la condicion y la operacion matematica
     public Vivo(): boolean{
         return this.vida > 0;
     }
