964ea3d50e7a6afcfe86ea1a5e5cf36745425ee7
macho991 <nachocremona264@gmail.com>
2026-09-21 16:27:16 -0300

    arma base terminada

--- src/ArmaBase.ts
+++ src/ArmaBase.ts
@@ -1,1 +1,17 @@
-export abstract
+export abstract class ArmaBase {
+    private bala : number;
+    private impacto : number;
+
+     
+    constructor(balaInicial:number, impactoInicial: number){
+        this.bala = balaInicial
+        this.impacto = impactoInicial 
+    }
+
+    public dispararBala(): number{
+        const impactoCausado = this.bala > 0 ? this.impacto : 0;
+        this.bala = this.bala > 0 ? this.bala -1 : 0;
+        return impactoCausado;
+    }
+
+}
